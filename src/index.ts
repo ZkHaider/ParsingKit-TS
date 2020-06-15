6d6b8a9caee27c7137067d9fe78bc455c022a8d6
ZkHaider <heathbbb7@gmail.com>
2020-06-14 19:03:49 -0700

    [Exports] - fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+import {Pair, Triple, Quadruple, Quintuple} from './utils/tuples';
+import {zip, zip2, zip3} from "./parsers/parser.zip";
+import {prefixWhile} from "./parsers/parser.prefix";
+import {parserMap, parserFlatMap} from "./parsers/parser.map";
+import {float, percentFloat} from "./parsers/parser.float";
+import {percentage} from "./parsers/parser.strings";
+import {never, always, literal, string, optionalSpaces} from "./parsers/parser.atomic";
+
+export { Pair, Triple, Quadruple, Quintuple };
+export { zip, zip2, zip3};
+export { prefixWhile };
+export { parserMap, parserFlatMap };
+export { float, percentFloat };
+export { percentage };
+export { never, always, literal, string, optionalSpaces };
+
 export type Optional<T> = T | null | undefined;
 export type ParseResult<T> = {
     match: Optional<T>,
